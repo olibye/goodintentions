05183c22468889b610ecb09cf2a36cf3139f0b60
Oli Bye <oli.github@mail-filter.com>
2012-02-02 21:23:14 +0000

    new: initial import of new project

---
+++ Intention/src/com/positiverobot/intentions/IntentionActivity.java
@@ -0,0 +1,13 @@
+package com.positiverobot.intentions;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IntentionActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
