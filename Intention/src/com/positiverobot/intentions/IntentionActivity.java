3fd9eaff0024861463d2d77c4bfee95e3811ffbf
Oli Bye <oli.github@mail-filter.com>
2012-02-02 21:58:45 +0000

    ref: move into Activity

--- Intention/src/com/positiverobot/intentions/IntentionActivity.java
+++ Intention/src/com/positiverobot/intentions/IntentionActivity.java
@@ -1,13 +1,67 @@
 package com.positiverobot.intentions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 
 public class IntentionActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	public class PInfo {
+		private String appname = "";
+		private String pname = "";
+		private String versionName = "";
+		private int versionCode = 0;
+		private Drawable icon;
+
+		private void prettyPrint() {
+			Log.v("INTENTIONS", appname + "\t" + pname + "\t" + versionName
+					+ "\t" + versionCode);
+		}
+	}
+
+	public ArrayList<PInfo> getPackages() {
+		ArrayList<PInfo> apps = getInstalledApps(false); /*
+														 * false = no system
+														 * packages
+														 */
+		final int max = apps.size();
+		for (int i = 0; i < max; i++) {
+			apps.get(i).prettyPrint();
+		}
+		return apps;
+	}
+
+	private ArrayList<PInfo> getInstalledApps(boolean getSysPackages) {
+		ArrayList<PInfo> res = new ArrayList<PInfo>();
+		PackageManager packageManager = getPackageManager();
+		List<PackageInfo> packs = packageManager.getInstalledPackages(0);
+		for (int i = 0; i < packs.size(); i++) {
+			PackageInfo p = packs.get(i);
+			if ((!getSysPackages) && (p.versionName == null)) {
+				continue;
+			}
+			PInfo newInfo = new PInfo();
+			newInfo.appname = p.applicationInfo.loadLabel(packageManager)
+					.toString();
+			newInfo.pname = p.packageName;
+			newInfo.versionName = p.versionName;
+			newInfo.versionCode = p.versionCode;
+			newInfo.icon = p.applicationInfo.loadIcon(packageManager);
+			res.add(newInfo);
+		}
+		return res;
+	}
 }
